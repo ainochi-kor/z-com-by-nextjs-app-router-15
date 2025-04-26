590e1b47698846663e0fe8b118613d924e608f29
ainochi-kor <kthrkdals@gmail.com>
2025-04-26 22:03:34 +0900

    프로필 페이지 및 검색 페이지 레이아웃 추가; 사용자 정보 표시 및 포스트 컴포넌트 통합; 스타일 및 CSS 모듈 정의

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import style from "./profile.module.css";
+import BackButton from "../_components/BackButton";
+import Post from "@/app/(afterLogin)/_components/Post";
 
-const UserNamePage = () => {
-  return <div>UserNamePage</div>;
-};
+export default function Profile() {
+  const user = {
+    id: "zerohch0",
+    nickname: "제로초",
+    image: "/5Udwvqim.jpg",
+  };
 
-export default UserNamePage;
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
+}
