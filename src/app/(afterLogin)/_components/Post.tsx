4ecd4afc06d3114f2949305bdd200f1336861736
ainochi-kor <kthrkdals@gmail.com>
2025-05-01 14:32:17 +0900

    @faker-js/faker 패키지 추가 및 관련 컴포넌트 구현; Post 및 PostImages 컴포넌트 수정; ActionButtons 스타일 개선; PhotoModalCloseButton 및 사진 모달 페이지 추가

--- src/app/(afterLogin)/_components/Post.tsx
+++ src/app/(afterLogin)/_components/Post.tsx
@@ -5,11 +5,17 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
 import ActionButtons from "@/app/(afterLogin)/_components/ActionButtons";
 import PostArticle from "./PostArticle";
+import { faker } from "@faker-js/faker";
+import PostImages from "@/app/(afterLogin)/_components/PostImages";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
-export default function Post() {
+type Props = {
+  noImage?: boolean;
+};
+
+export default function Post({ noImage }: Props) {
   const target = {
     postId: 1,
     User: {
@@ -19,8 +25,18 @@ export default function Post() {
     },
     content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   };
+
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() }
+    );
+  }
+
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
@@ -43,7 +59,9 @@ export default function Post() {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div>
+            <PostImages post={target} />
+          </div>
           <ActionButtons />
         </div>
       </div>
